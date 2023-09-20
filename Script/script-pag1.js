56f4de857eba856c997943939c149c80244e5e69
Gui <guiki53u19@gmail.com>
2023-09-20 18:52:25 -0300

    Adição dos planetas (fotos), movimento e animações

---
+++ Script/script-pag1.js
@@ -0,0 +1,9 @@
+function Função1(){
+    document.getElementById('teste').style.backgroundColor='rgb(60, 89, 219)'
+}
+function Função2(){
+    document.getElementById('teste1').style.backgroundColor='rgb(173, 132, 92)'
+}
+function Função3(){
+    document.getElementById('teste1').style.backgroundColor='rgb(49, 51, 151)'
+}
