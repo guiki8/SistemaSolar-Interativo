53eb373270afdfda064527f6358db792b593f800
Guilherme <guiki53u19@gmail.com>
2023-09-22 09:57:22 -0300

    Adição de responsividade

--- Script/script-pag1.js
+++ Script/script-pag1.js
@@ -1,9 +1,7 @@
 function Função1(){
     document.getElementById('teste').style.backgroundColor='rgb(60, 89, 219)'
 }
-function Função2(){
-    document.getElementById('teste1').style.backgroundColor='rgb(173, 132, 92)'
-}
-function Função3(){
-    document.getElementById('teste1').style.backgroundColor='rgb(49, 51, 151)'
+function Função_Escala(){
+    document.getElementById('planeta').style.height='50vmax'
+    document.getElementById('planeta').style.height='50vmax'
 }
