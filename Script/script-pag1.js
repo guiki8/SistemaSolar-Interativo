d5728b13350f7110b4727f95ca6840c7e4bb2854
Guilherme <guiki53u19@gmail.com>
2023-09-23 20:50:21 -0300

    Adição dos menus, melhora da responsividade, anéis de saturno e melhora na escala

--- Script/script-pag1.js
+++ Script/script-pag1.js
@@ -1,11 +1,98 @@
+/* Escala no monitor */
+
 function func_escala() {
     document.getElementById('scale_button_on').style.display = 'none'
     document.getElementById('scale_button_off').style.display = 'block'
-    document.getElementById('scale_ball').style.marginLeft = '50%'
+    document.getElementById('scale_button_on_cell').style.display = 'none'
+    document.getElementById('scale_button_off_cell').style.display = 'none'
+    document.getElementById('scale_ball').style.marginLeft = '6.25vmin'
 
     document.getElementById('planeta1').style.height = '605vmin'
     document.getElementById('planeta1').style.width = '605vmin'
     document.getElementById('planeta1').style.marginLeft = '-550vmin'
+
+    document.getElementById('planeta2').style.height = '2vmin'
+    document.getElementById('planeta2').style.width = '2vmin'
+
+    document.getElementById('planeta3').style.height = '5vmin'
+    document.getElementById('planeta3').style.width = '5vmin'
+
+    document.getElementById('planeta4').style.height = '5.2vmin'
+    document.getElementById('planeta4').style.width = '5.2vmin'
+
+    document.getElementById('planeta5').style.height = '2.9vmin'
+    document.getElementById('planeta5').style.width = '2.9vmin'
+
+    document.getElementById('planeta6').style.height = '60vmin'
+    document.getElementById('planeta6').style.width = '60vmin'
+    document.getElementById('planeta6').style.marginLeft = '5vw'
+
+    document.getElementById('planeta7').style.height = '50.4vmin'
+    document.getElementById('planeta7').style.width = '50.4vmin'
+    document.getElementById('planeta7').style.marginLeft = '10vw'
+
+    document.getElementById('planeta8').style.height = '22vmin'
+    document.getElementById('planeta8').style.width = '22vmin'
+    document.getElementById('planeta8').style.marginLeft = '10vw'
+
+    document.getElementById('planeta9').style.height = '21vmin'
+    document.getElementById('planeta9').style.width = '21vmin'
+    document.getElementById('planeta9').style.marginLeft = '5vw'
+}
+
+function func_escala_back() {
+    document.getElementById('scale_button_on').style.display = 'block'
+    document.getElementById('scale_button_off').style.display = 'none'
+    document.getElementById('scale_button_on_cell').style.display = 'none'
+    document.getElementById('scale_button_off_cell').style.display = 'none'
+    document.getElementById('scale_ball').style.marginLeft = '0.25vmin'
+
+    document.getElementById('planeta1').style.height = '15vmax'
+    document.getElementById('planeta1').style.width = '15vmax'
+    document.getElementById('planeta1').style.marginLeft = '0'
+
+    document.getElementById('planeta2').style.height = '3vmax'
+    document.getElementById('planeta2').style.width = '3vmax'
+
+    document.getElementById('planeta3').style.height = '5vmax'
+    document.getElementById('planeta3').style.width = '5vmax'
+
+    document.getElementById('planeta4').style.height = '4vmax'
+    document.getElementById('planeta4').style.width = '4vmax'
+
+    document.getElementById('planeta5').style.height = '4vmax'
+    document.getElementById('planeta5').style.width = '4vmax'
+
+    document.getElementById('planeta6').style.height = '10vmax'
+    document.getElementById('planeta6').style.width = '10vmax'
+    document.getElementById('planeta6').style.marginLeft = '3vw'
+
+    document.getElementById('planeta7').style.height = '8vmax'
+    document.getElementById('planeta7').style.width = '8vmax'
+    document.getElementById('planeta7').style.marginLeft = '3vw'
+
+    document.getElementById('planeta8').style.height = '8vmax'
+    document.getElementById('planeta8').style.width = '8vmax'
+    document.getElementById('planeta8').style.marginLeft = '3vw'
+
+    document.getElementById('planeta9').style.height = '9vmax'
+    document.getElementById('planeta9').style.width = '9vmax'
+    document.getElementById('planeta9').style.marginLeft = '3vw'
+}
+
+
+/* Escala no mobile */
+
+function func_escala_cell() {
+    document.getElementById('scale_button_on').style.display = 'none'
+    document.getElementById('scale_button_off').style.display = 'none'
+    document.getElementById('scale_button_on_cell').style.display = 'none'
+    document.getElementById('scale_button_off_cell').style.display = 'block'
+    document.getElementById('scale_ball').style.marginLeft = '6.25vmin'
+
+    document.getElementById('planeta1').style.height = '605vmin'
+    document.getElementById('planeta1').style.width = '605vmin'
+    document.getElementById('planeta1').style.marginTop = '-550vmin'
 
     document.getElementById('planeta2').style.height = '2vmin'
     document.getElementById('planeta2').style.width = '2vmin'
@@ -31,15 +118,17 @@ function func_escala() {
     document.getElementById('planeta9').style.height = '21vmin'
     document.getElementById('planeta9').style.width = '21vmin'
 }
-function func_escala_back() {
-    document.getElementById('scale_button_on').style.display = 'block'
+function func_escala_back_cell() {
+    document.getElementById('scale_button_on').style.display = 'none'
     document.getElementById('scale_button_off').style.display = 'none'
-    document.getElementById('scale_ball').style.marginLeft = '0'
-
+    document.getElementById('scale_button_on_cell').style.display = 'block'
+    document.getElementById('scale_button_off_cell').style.display = 'none'
+    document.getElementById('scale_ball').style.marginLeft = '0.25vmin'
+    
     document.getElementById('planeta1').style.height = '15vmax'
     document.getElementById('planeta1').style.width = '15vmax'
-    document.getElementById('planeta1').style.marginLeft = '0'
-
+    document.getElementById('planeta1').style.marginTop = '0'
+    
     document.getElementById('planeta2').style.height = '3vmax'
     document.getElementById('planeta2').style.width = '3vmax'
 
@@ -64,6 +153,10 @@ function func_escala_back() {
     document.getElementById('planeta9').style.height = '9vmax'
     document.getElementById('planeta9').style.width = '9vmax'
 }
+
+
+/* Pop up dos planetas */
+
 function func_sol() {
     document.getElementById('solpopup').style.display = 'block'
 }
--- Script/script-pag2.js
+++ Script/script-pag2.js
@@ -2,27 +2,4 @@ function func_escala() {
     document.getElementById('scale_button_on').style.display = 'none'
     document.getElementById('scale_button_off').style.display = 'block'
     document.getElementById('scale_ball').style.marginLeft = '50%'
-
-    document.getElementById('mercurio').style.animationDuration='0.5s'
-    document.getElementById('venus').style.animationDuration='1.25s'
-    document.getElementById('terra').style.animationDuration='2s'
-    document.getElementById('marte').style.animationDuration='3.7s'
-    document.getElementById('jupiter').style.animationDuration='23.7s'
-    document.getElementById('saturno').style.animationDuration='59s'
-    document.getElementById('urano').style.animationDuration='168s'
-    document.getElementById('netuno').style.animationDuration='329.5s'
-}
-function func_escala_back() {
-    document.getElementById('scale_button_on').style.display = 'block'
-    document.getElementById('scale_button_off').style.display = 'none'
-    document.getElementById('scale_ball').style.marginLeft = '0'
-
-    document.getElementById('mercurio').style.animationDuration='5s'
-    document.getElementById('venus').style.animationDuration='5s'
-    document.getElementById('terra').style.animationDuration='5s'
-    document.getElementById('marte').style.animationDuration='5s'
-    document.getElementById('jupiter').style.animationDuration='5s'
-    document.getElementById('saturno').style.animationDuration='5s'
-    document.getElementById('urano').style.animationDuration='5s'
-    document.getElementById('netuno').style.animationDuration='5s'
 }
